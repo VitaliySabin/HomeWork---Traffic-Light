20ac03005db7a4bf04ea3c693cad54fd7a30a38f
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2023-03-18 18:15:37 +0300

    Initial Commit

---
+++ HomeWork - Traffic Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork - Traffic Light
+//
+//  Created by Виталий Сабин on 18.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
